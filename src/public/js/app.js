efdbf6209eda26d4477f89d077c3bc8d5a495ccf
EC2 Default User <ec2-user@ip-172-31-40-36.ap-northeast-2.compute.internal>
2024-07-28 08:13:51 +0000

    Add webSocket contents

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);       // app.js의 socket은 server로의 연결을 의미함
+
+
+socket.addEventListener("open", () => {
+    console.log("Connected to server");
+});
+
+socket.addEventListener("message", (message) =>{
+    console.log("Just got this : ",message);
+});
+
+socket.addEventListener("close", () =>{
+    console.log("Disconnected to server");
+});
+
+setTimeout(()=>{
+    socket.send("hello from the browser before 10s");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,7 +9,31 @@ app.set("views", __dirname+"/views");
 app.use("/public", express.static(__dirname+"/public"));
 
 app.get("/", (req, res) => res.render("home") );
+app.get("/*", (req, res) => res.redirect("/") );
 
 const handleListen = () => console.log("Listening on http://43.201.207.187:3000");
 
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server  = http.createServer(app);
+
+const wss = new WebSocket.Server({server}); // http 서버 위에 web socket 서버를 올릴 수 있음
+
+
+// 아래 내용하고 똑같지만 아래처럼 사용하는 것이 훨씬 좋음 (익명함수)
+// wss.on("connection", handleConnection);
+// function handleConnection(socket){  // server.js의 socket은 연결된 browser를 의미함
+//     console.log(socket);
+// }
+
+wss.on("connection", (socket) => {
+
+    console.log("Connected to browser");
+    socket.on("close", ()=> console.log("Disconnected from the browser"));
+    socket.on("message", (message) => {
+        console.log(message.toString());
+    });
+    socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
